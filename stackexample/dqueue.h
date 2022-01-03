58bff94b05af385035bd9793be4d4df426745301
Dennis Benkhardt <dbenkhar@c2s1d1.42wolfsburg.de>
2022-01-03 11:53:41 +0100

    dk where I'm pushing to

--- stackexample/dqueue.h
+++ stackexample/dqueue.h
@@ -6,7 +6,7 @@
 /*   By: dbenkhar <dbenkhar@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 10:22:22 by dbenkhar          #+#    #+#             */
-/*   Updated: 2022/01/03 10:29:23 by dbenkhar         ###   ########.fr       */
+/*   Updated: 2022/01/03 11:53:32 by dbenkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ typedef struct s_elem{
 	struct s_elem	*bot;
 }	t_elem;
 
-void	printlist_top(t_elem *top);
 t_elem	*create_elem_ontop(int value, t_elem *top);
 t_elem	*create_elem_onbot(int value, t_elem *bot);
 
